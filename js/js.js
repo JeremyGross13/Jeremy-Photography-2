b1c915fc6bcbc72c7ca20b16c460557852a81afc
Jeremy <jeremy.gross@etu.univ-amu.fr>
2023-01-20 14:45:42 +0100

    ajout popup

--- js/js.js
+++ js/js.js
@@ -6,4 +6,7 @@ function myFunction() {
         x.className = "nav";
     }
 }
-        
+
+function popup() {
+    w = open("html/popup.html", 'Avis', 'width=500,height=300,toolbar=no,scrollbars=no,resizable=no,location=no,top=200,left=500');
+}
