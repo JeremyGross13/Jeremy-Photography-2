7d0e668cf4670ca6b1b8f474f7ad8a70a1d920d5
Jeremy <jeremy.gross@etu.univ-amu.fr>
2023-01-20 16:45:17 +0100

    ajout bouton retour vers le haut

--- js/js.js
+++ js/js.js
@@ -6,3 +6,16 @@ function myFunction() {
         x.className = "nav";
     }
 }
+
+jQuery(function () {
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('#scrollUp').css('right', '10px');
+            } else {
+                $('#scrollUp').removeAttr('style');
+            }
+
+        });
+    });
+});
